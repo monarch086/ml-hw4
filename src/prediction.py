27dbf59e68e2a9af844ad756681a3c2b1e6a1e4b
Mykhailo Halaida <galaida.m@gmail.com>
2023-12-06 22:04:34 +0200

    notebook updates, code style changes

--- src/prediction.py
+++ src/prediction.py
@@ -1,8 +1,10 @@
-import pandas as pd
+'''Script for loading the persisted model and making predictions'''
+
 import pickle
-from source import clean_text, process_text
+import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.decomposition import TruncatedSVD
+from source import clean_text, process_text
 
 MODEL_NAME = "MOC_KNC"
 
@@ -17,13 +19,13 @@ input_df = pd.DataFrame.from_dict({
 
 
 input_df['cleaned_text'] = input_df['raw_text'].apply(clean_text)
-input_df['processed_text'] = input_df['cleaned_text'].apply(clean_text)
+input_df['processed_text'] = input_df['cleaned_text'].apply(process_text)
 input_df = input_df.drop(columns=['raw_text', 'cleaned_text'])
 
 # print(input_df)
 
 with open('models/Vectorizer.pkl', 'rb') as f:
-    tfidf_vectorizer: TfidfVectorizer = pickle.load(f);
+    tfidf_vectorizer: TfidfVectorizer = pickle.load(f)
 
 
 tfidf_matrix = tfidf_vectorizer.transform(input_df['processed_text'])
@@ -31,7 +33,7 @@ tfidf_matrix = tfidf_vectorizer.transform(input_df['processed_text'])
 print(tfidf_matrix)
 
 with open('models/SVD.pkl', 'rb') as f:
-    svd_transformer: TruncatedSVD = pickle.load(f);
+    svd_transformer: TruncatedSVD = pickle.load(f)
 
 svd_dimensions = svd_transformer.transform(tfidf_matrix)
 
@@ -39,10 +41,13 @@ svd_dimensions = svd_transformer.transform(tfidf_matrix)
 
 with open(f"models/{MODEL_NAME}.pkl", "rb") as f:
     model = pickle.load(f)
-    
+
 
 result = model.predict(svd_dimensions)
 print(result)
 
-result = dict(zip(['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate'], result.flatten()))
+result = dict(
+    zip(['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate'],
+    result.flatten())
+)
 print(result)
